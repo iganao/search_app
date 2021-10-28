f55dd99316c546510dde40502644063e12836c18
iganao <naokiiga0504@gmail.com>
2021-10-28 20:55:41 +0900

    重複の解決

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,16 +2,21 @@ class ProductsController < ApplicationController
   before_action :search_product, only: [:index, :search]
 
   def index
-    @products = Product.all  # 全商品の情報を取得
+    @products = Product.all
+    set_product_column
   end
 
   def search
-    @results = @p.result.includes(:category)  # 検索条件にマッチした商品の情報を取得
+    @results = @p.result.includes(:category)
   end
 
   private
 
   def search_product
-    @p = Product.ransack(params[:q])  # 検索オブジェクトを生成
+    @p = Product.ransack(params[:q])
+  end
+  
+  def set_product_column
+    @product_name = Product.select("name").distinct  # 重複なくnameカラムのデータを取り出す
   end
 end
